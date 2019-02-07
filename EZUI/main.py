8b2049f82dad614f6184fd8098ea3be41e501e0b
jonay2000 <jonabent@gmail.com>
2019-02-07 14:59:37 +0100

    added recursive surface drawing

--- EZUI/main.py
+++ EZUI/main.py
@@ -1,25 +1,35 @@
-
 import pyglet
 from pyglet.gl import *
 import json
 import os
+from . import surface
 
 path = os.path.dirname(os.path.abspath(__file__))
 
 
-class Window(pyglet.window.Window):
+class Window(pyglet.window.Window, surface.Surface):
     def __init__(self):
         with open(os.path.join(path, "config.json")) as f:
             self.configfile = json.load(f)
 
-        super().__init__(
+        pyglet.window.Window.__init__(
+            self,
             self.configfile["window"]["defaultwidth"],
-            self.configfile["window"]["defaultheight"]
+            self.configfile["window"]["defaultheight"],
+            resizable=True
+        )
+        surface.Surface.__init__(
+            self,
+            "root",
+            (0, 0),
+            (100, 100)
         )
 
         # enable transparency
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        # make polygons filled
+        glPolygonMode(GL_FRONT, GL_FILL)
         # set background color
         glClearColor(1.0, 1.0, 1.0, 1.0)
 
@@ -40,13 +50,11 @@ class Window(pyglet.window.Window):
         draws all content to screen
         """
         glClear(GL_COLOR_BUFFER_BIT)
-        label = pyglet.text.Label(
-            'Hello, world',
-            font_name='Times New Roman',
-            font_size=36,
-            x=self.width//2, y=self.height//2,
-            anchor_x='center', anchor_y='center',
-            color=(0, 0, 0, 255)
-        )
 
-        label.draw()
+        glPushMatrix()
+        # translate to make the origin top left
+        glTranslatef(0, self.height, 0)
+        glScalef(1, -1, 1)
+
+        self.draw()
+        glPopMatrix()
--- test.py
+++ test.py
@@ -3,4 +3,16 @@ import EZUI
 
 
 window = EZUI.Window()
-window.run()
+window.addChildren(
+    EZUI.Surface("leftroot", (0, 0), (50, 100), subsufaces=[
+        EZUI.Surface("leftrootbottom", (0, 50), (100, 50), subsufaces=[
+            EZUI.Surface("leftrootbottomleft", (0, 0), (50, 100)),
+            EZUI.Surface("leftrootbottomright", (50, 0), (50, 100)),
+        ]),
+    ]),
+    EZUI.Surface("rightroot", (50, 0), (50, 100), subsufaces=[
+        EZUI.Surface("rightroottop", (0, 0), (100, 70)),
+        EZUI.Surface("rightrootbottom", (0, 70), (100, 30)),
+    ])
+)
+EZUI.Window.run()
